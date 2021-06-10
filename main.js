fa813f67dc121ca5dc96ed6d27272d954f0b6cdd
matthias-vanermen <r0813580@student.thomasmore.be>
2021-06-10 21:11:09 +0200

    Verschillende charts

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { createPublicKey } = require('crypto')
 const { app, BrowserWindow, Menu, Accelerator } = require('electron')
 const path = require('path')
 
-function createWindow () {
-	// Create the browser window.
+function createMainWindow () {
+	// Create the main browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,13 +12,32 @@ function createWindow () {
 
 	// and load the index.html of the app.
   win.loadFile('index.html')
+  win.on('closed', function(){
+    app.quit();
+  })
+}
+
+function createWindow (pathHtml) {
+	// Create the main browser window.
+  win = new BrowserWindow({
+    width: 600,
+    height: 450,
+  })
+
+	// and load the index.html of the app.
+  win.loadFile(pathHtml)
+
+  // Garbage collection
+  win.on('close', function(){
+    win = null;
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createMainWindow()
   //build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   //Insert menu
@@ -27,59 +46,105 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
   })
 })
-
-//Handle create tab
-function createTabWindow(){
-  	// Create tab window.
-    const win = new BrowserWindow({
-      width: 600,
-      height: 450,
-    })
-  
-    // and load the index.html of the app.
-    win.loadFile('views/tab.html')
-}
-
-//Handle create section
-function createSectionWindow(){
-  // Create section window.
-  const win = new BrowserWindow({
-    width: 600,
-    height: 450,
-  })
-
-  // and load the index.html of the app.
-  win.loadFile('views/section.html')
-}
 
 // Create menu template
 const mainMenuTemplate = [
   {
     label:'Tab maken',
     click(){
-      createTabWindow();
+      // Create tab window.
+      createWindow('views/tab.html');
     }
   },
   {
     label: 'Section maken',
     click(){
-      createSectionWindow();
+      // Create section window
+      createWindow('views/section.html');
     }
   },
   {
     label: 'Graph maken',
     submenu:[
       {
-        label: 'Graph 1'
+        label: 'Demographic Chart',
+        click(){
+          // Create Demographic Chart
+          createWindow('views/Charts/chartDemograpic.html');
+        }
       },
       {
-        label: 'Graph 2'
+        label: 'Densityplots Chart',
+        click(){
+          // Create Density plot Chart
+          createWindow('views/Charts/chartDensityplots.html');
+        }
       },
       {
-        label: 'Graph 3'
+        label: 'Distribution Chart',
+        click(){
+          // Create Distribution Chart
+          createWindow('views/Charts/chartDistribution.html');
+        }
+      },
+      {
+        label: 'Dyads Chart',
+        click(){
+          // Create Dyads Chart
+          createWindow('views/Charts/chartDyads.html');
+        }
+      },
+      {
+        label: 'List Chart',
+        click(){
+          // Create List Chart
+          createWindow('views/Charts/chartList.html');
+        }
+      },
+      {
+        label: 'Multiplechoicebar Chart',
+        click(){
+          // Create Multiple choice bar Chart
+          createWindow('views/Charts/chartMultiplechoicebar.html');
+        }
+      },
+      {
+        label: 'Multipletopicbar Chart',
+        click(){
+          // Create Multiple topic bar Chart
+          createWindow('views/Charts/chartMultipletopicbar.html');
+        }
+      },
+      {
+        label: 'Simplebar Chart',
+        click(){
+          // Create Simple bar Chart
+          createWindow('views/Charts/chartSimplebar.html');
+        }
+      },
+      {
+        label: 'Traffic Chart',
+        click(){
+          // Create Traffic Chart
+          createWindow('views/Charts/chartTraffic.html');
+        }
+      },
+      {
+        label: 'Triads Chart',
+        click(){
+          // Create Triads Chart
+          createWindow('views/Charts/chartTriads.html');
+        }
+      },
+      {
+        label: 'Wordcloud Chart',
+        click(){
+          // Create Wordcloud Chart
+          createWindow('views/Charts/chartWordcloud.html');
+        }
       }
     ]
   },
@@ -91,7 +156,32 @@ const mainMenuTemplate = [
       app.quit();
     }
   }
-]
+];
+
+// If mac, add empty object to menu
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+// Add develop tools
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' :
+        'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role:'reload'
+      }
+    ]
+  });
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -99,7 +189,3 @@ const mainMenuTemplate = [
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
